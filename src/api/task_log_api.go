104ef95bf92ba1f5fb19017ccfd928bf7c2310e4
pengshiyu <1940607002@qq.com>
2024-03-24 14:44:08 +0800

    fix

--- src/api/task_log_api.go
+++ src/api/task_log_api.go
@@ -7,14 +7,32 @@ import (
 	"github.com/mouday/cron-admin/src/vo"
 )
 
+type PageForm struct {
+	Page   int `json:"page"`
+	Size   int `json:"size"`
+	Status int `json:"status"`
+}
+
+func (pageForm PageForm) PageOffset() int {
+	return (pageForm.Page - 1) * pageForm.Size
+}
+
 func GetTaskLogList(ctx *gin.Context) {
 	// taskList := service.GetTaskList()
 	// database
+	params := &PageForm{
+		Page:   1,
+		Size:   10,
+		Status: 0,
+	}
+
+	ctx.BindJSON(&params)
+
 	db := config.GetDB()
 
 	taskList := []model.TaskLogModel{}
 
-	db.Model(&model.TaskLogModel{}).Find(&taskList)
+	db.Model(&model.TaskLogModel{}).Find(&taskList).Limit(params.Size).Offset(params.PageOffset())
 
 	// ctx.JSON(http.StatusOK, taskList)
 	vo.Success(ctx, taskList)
