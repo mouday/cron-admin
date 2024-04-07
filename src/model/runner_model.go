0a000f64bd0a17d6696f95a621d4f3f1970f8753
pengshiyu <1940607002@qq.com>
2024-04-07 20:51:41 +0800

    fix

---
+++ src/model/runner_model.go
@@ -0,0 +1,18 @@
+package model
+
+import "github.com/mouday/cron-admin/src/utils"
+
+type RunnerModel struct {
+	Id          uint            `json:"-"`
+	Title       string          `json:"title"`
+	Host        string          `json:"host"`
+	AccessToken string          `json:"accessToken"`
+	Status      bool            `json:"status"`
+	CreateTime  utils.LocalTime `gorm:"type:datetime;autoCreateTime" json:"createTime"`
+	UpdateTime  utils.LocalTime `gorm:"type:datetime;autoUpdateTime" json:"updateTime"`
+}
+
+// 自定义表名
+func (TaskModel) TableName() string {
+	return "tb_runner"
+}
--- src/model/task_model.go
+++ src/model/task_model.go
@@ -3,12 +3,14 @@ package model
 import "github.com/mouday/cron-admin/src/utils"
 
 type TaskModel struct {
-	Id         uint            `json:"-"`
-	Title      string          `json:"title"`
-	Cron       string          `json:"cron"`
-	Url        string          `json:"url"`
+	Id       uint   `json:"-"`
+	TaskId   string `gorm:"index" json:"taskId"`
+	Title    string `json:"title"`
+	RunnerId string `json:"runnerId"`
+	TaskName string `json:"taskName"`
+	Cron     string `json:"cron"`
+	// Url        string          `json:"url"`
 	Status     bool            `json:"status"`
-	TaskId     string          `gorm:"index" json:"taskId"`
 	CreateTime utils.LocalTime `gorm:"type:datetime;autoCreateTime" json:"createTime"`
 	UpdateTime utils.LocalTime `gorm:"type:datetime;autoUpdateTime" json:"updateTime"`
 }
